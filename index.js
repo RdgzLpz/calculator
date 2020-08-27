8fd3c7e9d837d53ea4e2712aed5c753882237731
Oscar Rodriguez <varios_varios@outlook.com>
2020-08-26 20:08:22 -0400

    started re-writing JS

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-let number = document.querySelectorAll('button');
+let screen;
+function start( ) {
+     screen = document.querySelector('#screen');
+    let buttons = document.querySelectorAll('button');
 
-function handleClick(event) {
-    alert('CLICK')
+    window.addEventListener('keydown', handleInputByKeyboard);
+
+    for (let i in buttons) {
+        buttons[ i ].addEventListener('click', handleInput);
+    } 
+
 }
+/*  Handle inputs */
+function handleInput(event) {       
+    let input = event.target.innerHTML;
+    let isNumber = /\d/.test( input );
+
+    if (isNumber) addToScreen( input );
+}
+
+function handleInputByKeyboard(event) {
+    let input = event.key;
+    let isNumber = /\d/.test( input );
+    if (isNumber)  addToScreen(input);
+}
+/*  Add values to screen  */
+function addToScreen (input ) {
+    screen.innerHTML += input;
+}
+
+window.addEventListener('load', start);
